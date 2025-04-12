5c39958feab016ad8d469878758d6a0d2af7bce8
RubyRaven6 <swampertspawner@gmail.com>
2025-04-13 01:27:19 +0800

    Fixed typo on my end

--- include/constants/battle_string_ids.h
+++ include/constants/battle_string_ids.h
@@ -732,15 +732,15 @@
 #define STRINGID_PKMNDYNAMAXED                        730
 #define STRINGID_PKMNGIGANTAMAXED                     731
 #define STRINGID_TIMETODYNAMAX                        732
-#define STRINGID_TIMETOGIGANTAMAX                     734
-#define STRINGID_QUESTIONFORFEITBATTLE                735
-#define STRINGID_FORFEITBATTLEGAVEMONEY               736
+#define STRINGID_TIMETOGIGANTAMAX                     733
+#define STRINGID_QUESTIONFORFEITBATTLE                734
+#define STRINGID_FORFEITBATTLEGAVEMONEY               735
 
 //  Ruby moves
-#define STRINGID_GIPHANT_CAPTURE_FAIL                 737
-#define STRINGID_GIPHANT_CAPTURE_SUCCESS              738
+#define STRINGID_GIPHANT_CAPTURE_FAIL                 736
+#define STRINGID_GIPHANT_CAPTURE_SUCCESS              737
 
-#define BATTLESTRINGS_COUNT                           739
+#define BATTLESTRINGS_COUNT                           738
 
 // This is the string id that gBattleStringsTable starts with.
 // String ids before this (e.g. STRINGID_INTROMSG) are not in the table,
